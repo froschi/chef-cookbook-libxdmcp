f114d6eb6d775449eb1a53383e81aa5b431f91ed
Thorsten Fischer <thorsten@froschi.org>
2012-06-19 00:44:07 +0200

    Amended for use on lucid, precise.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,11 @@
-packages = %w/
-  libxdmcp6
-/
+packages = Array.new
+
+case node[:lsb][:codename]
+when "lucid", "precise"
+  packages |= %w/
+    libxdmcp6
+  /
+end
 
 packages.each do |pkg|
   package pkg do
